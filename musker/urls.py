6faf0e0b26939cdd87ca0a7703376e76dd545d9e
Raine <pyae00254@gmail.com>
2023-12-13 10:10:33 +0630

    login & logout

--- musker/urls.py
+++ musker/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.home, name = 'home'),
     path('profile_list/',views.profile_list, name= 'profile_list'),
     path('profile/<int:pk>', views.profile, name= 'profile'),
+    path('login_user/',views.login_user, name = 'login_user'),
+    path('logout_user/',views.logout_user, name = 'logout_user'),
 ]
